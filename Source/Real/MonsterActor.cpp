0dc746ef05ca0af4938b0953b4e0861cb21098b5
Park Jin Woo <pjw1480@naver.com>
2021-08-24 18:04:50 +0900

    빵꾸 원 패턴

--- Source/Real/MonsterActor.cpp
+++ Source/Real/MonsterActor.cpp
@@ -2,6 +2,9 @@
 
 
 #include "MonsterActor.h"
+
+#include <concrt.h>
+
 #include "Bullet.h"
 #include "UObject/ConstructorHelpers.h"
 #include "Components/StaticMeshComponent.h"
@@ -146,25 +149,6 @@ void AMonsterActor::FireShot()
 		ABullet* monsterBullet = gm->BulletPooler->GetPooledBullet();
 		if (monsterBullet && World)
 		{
-			AActor* player = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
-			FVector Dir = player->GetActorLocation() - this->GetActorLocation();
-			// Dir.Normalize(); // 벡터
-			//
-			// const FVector Movement = Dir * BulletSpeed; //
-			// // 가지고있는 액터 누구?
-			// monsterBullet->SetOwnerActor(this);
-			// // 가지고있는 총알위치
-			// monsterBullet->SetActorLocation(GetActorLocation());
-			// //총알 속도
-			// monsterBullet->SetVelocity(Movement);
-			// // 알아서 살아지게하고
-			// monsterBullet->SetLifeSpan();
-			// // 활성화시킨다.
-			// monsterBullet->SetActive(true);
-
-
-			//원형 36은 변수로 넣기
-
 			for (int i = 0; i < 36; i++)
 			{
 				monsterBullet = gm->BulletPooler->GetPooledBullet();
@@ -186,16 +170,70 @@ void AMonsterActor::FireShot()
 				monsterBullet->SetOwnerActor(this);
 				// 가지고있는 총알위치
 				monsterBullet->SetActorLocation(Location);
+
 				//총알 속도
 				monsterBullet->SetVelocity(FMovement);
 				// 알아서 살아지게하고
 				monsterBullet->SetLifeSpan();
 				// 활성화시킨다.
 				monsterBullet->SetActive(true);
+				if (i > 30)
+				{
+					monsterBullet->SetActive(false);
+				}
 			}
 
 
-			// 4방향으로 발사
+			//AActor* player = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+			//FVector Dir = player->GetActorLocation() - this->GetActorLocation();
+			// Dir.Normalize(); // 벡터
+			//
+			// const FVector Movement = Dir * BulletSpeed; //
+			// // 가지고있는 액터 누구?
+			// monsterBullet->SetOwnerActor(this);
+			// // 가지고있는 총알위치
+			// monsterBullet->SetActorLocation(GetActorLocation());
+			// //총알 속도
+			// monsterBullet->SetVelocity(Movement);
+			// // 알아서 살아지게하고
+			// monsterBullet->SetLifeSpan();
+			// // 활성화시킨다.
+			// monsterBullet->SetActive(true);
+
+			//
+			// //원형 36은 변수로 넣기
+			//
+			// for (int i = 0; i < 36; i++)
+			// {
+			// 	monsterBullet = gm->BulletPooler->GetPooledBullet();
+			// 	// 몬스터 위치
+			// 	FVector Location = GetActorLocation();
+			// 	// 원 (x,y)
+			// 	FVector Circledir(cos((2 * PI * i) / 36), sin((2 * PI * i) / 36), 0.f);
+			// 	// 반지금이 100인 원(x,y)
+			// 	Circledir *= 100;
+			// 	// 내위치에 그 위치를 더해서
+			// 	Location += Circledir;
+			// 	// 방향은 먼위치 - 내위치
+			// 	FVector Dir = Location - GetActorLocation();
+			// 	// 방향벡터 
+			// 	Dir.Normalize();
+			//
+			// 	const FVector FMovement = Dir * BulletSpeed; //
+			// 	// 가지고있는 액터 누구?
+			// 	monsterBullet->SetOwnerActor(this);
+			// 	// 가지고있는 총알위치
+			// 	monsterBullet->SetActorLocation(Location);
+			// 	//총알 속도
+			// 	monsterBullet->SetVelocity(FMovement);
+			// 	// 알아서 살아지게하고
+			// 	monsterBullet->SetLifeSpan();
+			// 	// 활성화시킨다.
+			// 	monsterBullet->SetActive(true);
+			// }
+
+
+			// 4개 발사
 			// for(int i = 0 ; i<5;i++)
 			// {
 			// 	monsterBullet = gm->BulletPooler->GetPooledBullet();
